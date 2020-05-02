6fe2a5147eb54af455600570ac891cda34514164
leonard <leonard.najman@gmail.com>
2020-05-03 01:58:12 +0200

    list+^pile update

--- TP2/src/main/java/tp2/Liste.java
+++ TP2/src/main/java/tp2/Liste.java
@@ -11,4 +11,36 @@ package tp2;
  */
 public class Liste {
     
+    public Liste(Object ob){
+    iOb = ob;
+    System.out.println("initialisation= "+iOb);
+    }
+    
+    public void dernier(){
+    int dernier=objectArray.length-1;
+    }
+    
+    
+    public void Ajouter (Object ob){
+    iOb=ob;
+    ////utilisation de la methode add
+    Liste.add(ObjectAjouter);
+    System.out.println(ObjectAjouter);
+    }
+    
+    //possitionement sur premier element
+    public int Premier ()
+    {
+    //recuperation du 1er element (position 0)
+        PremierElement=Liste.get(0);
+    }
+    
+    //Positionement sur dernier element
+    public int Dernier()
+    {
+        //recuperation de la taille de la liste pour recup le dernier
+        DernierElement=Liste.get(Liste.size()-1);
+    }
+    
+    
 }
