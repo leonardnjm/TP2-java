ac2e422480a8da6e8ef71f0f166b937e86b1395f
leonard <leonard.najman@gmail.com>
2020-05-03 18:48:05 +0200

    Hanoi class

--- TP2/src/main/java/Hanoi/Hanoi.java
+++ TP2/src/main/java/Hanoi/Hanoi.java
@@ -4,11 +4,32 @@
  * and open the template in the editor.
  */
 package Hanoi;
+import tp2.PileEntiers;
 
 /**
  *
  * @author Alexandre
  */
 public class Hanoi {
+    private Integer iDiameter;
+    public PileEntiers iPE1;
+    public PileEntiers iPE2;
+    public PileEntiers iPE3;
+    
+    public Hanoi(){
+    iPE1= new PileEntiers();
+    iPE2= new PileEntiers();
+    iPE3= new PileEntiers();
+    }
+    
+    public void setDiameter(Integer iInt){
+    iDiameter=iInt;
+    }
+    
+    public Integer getDiameter(){
+    
+        return iDiameter;
+    }
+    
     
 }
