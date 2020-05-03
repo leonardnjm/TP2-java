0f1a136968e3791612146d23310e54b7a0064c67
Alexandre <alexhamelin99@gmail.com>
2020-05-03 20:41:31 +0200

    petite correction

--- TP2/src/main/java/Hanoi/HanoiTest.java
+++ TP2/src/main/java/Hanoi/HanoiTest.java
@@ -11,23 +11,30 @@ package Hanoi;
  */
 public class HanoiTest {
     
-        
+    public void diques(int n){
+        Hanoi iHanoi = new Hanoi();
+        for(int i=0; i<n; i++){
+            int iInt = (int)(Math.random()*100);
+            iHanoi.setDiameter(iInt);
+            iHanoi.iPE1.empiler(iHanoi.getDiameter());
+        }
+        iHanoi.iPE1.sort
+    }
     
-    static void tourHanoi(int n, String pile1, String pile2, String pile3, int compteur){
+    static void tourHanoi(int n, String pile1, String pile2, String pile3){
         if (n==0){
-            System.out.println("nombre de coup: " + compteur);
             return;
         }
-        tourHanoi(n-1, pile1, pile3, pile2, compteur);
+        tourHanoi(n-1, pile1, pile3, pile2);
         System.out.println("Mouvement du disque " + n + " pile de départ " + pile1 + " pile d'arrivée " + pile3);
-        tourHanoi(n-1, pile2, pile1, pile3, compteur);
+        tourHanoi(n-1, pile2, pile1, pile3);
     }
     
     public static void main(String[] args) {
         java.util.Scanner entree =   new java.util.Scanner(System.in);
         System.out.println("Entrez le nombre de disque: ");
 	int n = entree.nextInt();
-        int compteur= 0;
-        tourHanoi(n, "pile1", "pile2", "pile3", compteur++);
+        disque(n);
+        tourHanoi(n, "pile1", "pile2", "pile3");
 	}
 }
